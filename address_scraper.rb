c0bdac5221304302919053895ab5ef8f3ae975aa
Marco Antonio <marcobaraoneves@gmail.com>
2021-03-21 21:48:51 +0000

    v1 working

--- address_scraper.rb
+++ address_scraper.rb
@@ -1,2 +1,35 @@
 class AddressScraper
+	def initialize
+		@addresses = []
+ 	   @regex = /(Av(?:enida|\.)?|R(?:ua|\.)?)\s([\p{L}\d\s\(\)\-\/\.]+),\s(\d+\w*)\s([\p{L}\d\s\(\)\-\/\.]+\s)?-\s([\p{L}\d\s\(\)\-\/\.]+),\s([\p{L}\s]+)-\s(\w{2}),\s(\d{5}-?\d{3})/i
+	end
+
+    def scrap(text)
+        @addresses = text.scan(@regex)
+    end
+    
+    def print
+        @addresses.each_with_index do |address, index|
+            puts '------------'
+            puts "Endereço #{index + 1}"
+            puts "Tipo: #{address[0]}"
+			puts "Nome: #{address[1]}"
+			puts "Número: #{address[2]}"
+			puts "Complemento: #{(!address[3] ? 'N/A': address[3])}"
+			puts "Bairro: #{address[4]}"
+			puts "Cidade: #{address[5]}"
+			puts "Estado: #{address[6]}"
+			puts "CEP: #{address[7]}"
+			puts '------------'
+        end
+    end
 end
+
+# Tipo: 
+# Nome
+# número
+# complemento
+# bairro
+# cidade
+# estado
+# cep
--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
-puts "main"
+load 'address_scraper.rb'
+
+text = gets()
+puts "\n"
+
+addressScraper = AddressScraper.new
+
+addressScraper.scrap(text)
+addressScraper.print
+
+"""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Av. Eng. Eusébio Stevaux, 823 - Santo Amaro, São Paulo - SP, 04696-000 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Av. Eng. Eusébio Stevaux, 823 Sala 22 - Santo Amaro, São Paulo - SP, 04696-000
+""" 
